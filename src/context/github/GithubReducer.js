6b73ef2bbf353f85f76ce651a7759fab831ab88c
Graham <graham.moss@hotmail.com>
2021-12-31 00:47:08 -0500

    user page component

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,14 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       }
+
+    case 'GET_USER':
+      console.log('action', action)
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      }
     case 'SET_LOADING':
       return {
         ...state,
